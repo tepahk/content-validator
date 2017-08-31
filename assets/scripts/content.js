88d1580351e6dc36338b905ac1889a6151e66cde
Tessa Pahkamaa <tessa.pahkamaa@rosetta.com>
2017-08-31 10:07:16 -0700

    Updating clean string function to accomodate non-friendly regex chars

--- assets/scripts/content.js
+++ assets/scripts/content.js
@@ -1,7 +1,7 @@
 var validationStatus = {};
 
 // Utility to replace non-breaking space characters with regular when validating them
-function replaceNbsps(str) {
+function cleanString(str, cleanForRegex) {
     // Replaces with a single space: 
     // - non-breaking space characters e.g. " "
     // - encoded non-breaking spaces e.g. "&nsbp;"
@@ -9,10 +9,14 @@ function replaceNbsps(str) {
 
     var newStr = str,
         regex = new RegExp(String.fromCharCode(160), "g");
+    newStr = newStr.trim();
     newStr = newStr.replace(regex, " ");
     newStr = newStr.replace(/  /g, " ");
     newStr = newStr.replace(/&nbsp;/g, " ");
-    newStr = newStr.trim();
+    if( cleanForRegex ){
+        // Escape all regex characters
+        newStr = newStr.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+    }
 
     return newStr;
 }
@@ -24,53 +28,49 @@ function findAndReplace(searchText, replacement, searchNode, id) {
         return;
     }
 
-    searchText = replaceNbsps(searchText);
-
     // This regex current ignores case!!
     var regex = typeof searchText === 'string' ?
         new RegExp('^'+searchText+'$', 'gi') : searchText,
         childNodes = (searchNode || document.body).childNodes,
         cnLength = childNodes.length,
-        excludes = 'html,head,style,title,link,meta,script,object,iframe';
+        excludes = ['html','head','style','title','link','meta','script','object','iframe'];
 
     while (cnLength--) {
         var currentNode = childNodes[cnLength],
             currentNodeText = currentNode.textContent;
 
-        currentNodeText = replaceNbsps(currentNodeText);
-        if (currentNode.nodeType === 1 && (excludes + ',').indexOf(currentNode.nodeName.toLowerCase() + ',') === -1) {
+        currentNodeText = cleanString(currentNodeText);
+
+        if (currentNode.nodeType === 1 && excludes.indexOf(currentNode.nodeName.toLowerCase()) === -1) {
             findAndReplace(searchText, replacement, currentNode, id);
         }
-        if (currentNode.nodeType !== 3 || !regex.test(currentNodeText)) {
+        // Checks if node is NOT text
+        // Checks if node does not match the search string regex
+        // Checks if node has already been validated (TODO)
+
+        if (currentNode.nodeType !== 3 || !regex.test(currentNodeText) ) {
+            // Abandon ship and move on to the next item in the loop
             continue;
         }
 
-        if (currentNodeText.match(regex)) {
-            //console.log(currentNodeText)
+        // Update validationStatus object to know if we found this instance of the string
+        validationStatus[id] = true;
 
-            if( id ){
-                console.log(id, currentNode, currentNode.parentNode)
-                validationStatus[id] = true;
-            }
+        // Actually replace content with decorative span
+        var parent = currentNode.parentNode,
+            frag = (function() {
+                var html = currentNodeText.replace(regex, replacement),
+                    wrap = document.createElement('span'),
+                    frag = document.createDocumentFragment();
+                wrap.innerHTML = html;
+                while (wrap.firstChild) {
+                    frag.appendChild(wrap.firstChild);
+                }
+                return frag;
+            })();
+        parent.insertBefore(frag, currentNode);
+        parent.removeChild(currentNode);
 
-            // TODO ensure it matches the WHOLE THING not just one word e.g. "about"
-
-            // Actually replace content
-            var parent = currentNode.parentNode,
-                frag = (function() {
-                    var html = currentNodeText.replace(regex, replacement),
-                        wrap = document.createElement('span'),
-                        frag = document.createDocumentFragment();
-                    wrap.innerHTML = html;
-                    while (wrap.firstChild) {
-                        frag.appendChild(wrap.firstChild);
-                    }
-                    return frag;
-                })();
-            parent.insertBefore(frag, currentNode);
-            parent.removeChild(currentNode);
-
-        }
     }
 }
 
@@ -92,20 +92,15 @@ chrome.runtime.onMessage.addListener(function(workbook) {
                 if (thisCell.v) {
                     // Check for this content on the page
                     var value = thisCell.v;
-                    findAndReplace(value, '<span class="content-validated" title="' + cell + '">' + value + '</span>', null, cell);
+                    findAndReplace(cleanString(value, true), '<span class="content-validated" title="' + cell + '">' + value + '</span>', null, cell);
 
                     // Add this item to the report
-                    var stringWasFound = false;
-
-                    console.log( validationStatus[cell] )
-
-                    // if( nodesFound.length == 0 ){
-                    //     console.warn( 'Did not find', cell, ' - ', '"'+thisCell.v+'"' )
-                    // }
-
-                    // TODO Only push exists if it does exist
                     report.push( [value, validationStatus[cell]] )
-  
+
+                    // TODO: still not finding the 4 words in the intro section?
+                    // TODO: still not finding contnet in the global nav
+                    // TODO: style status cells in workbook for strings that are not found
+                    // TODO:  only find the first instance of a string, if it has already been found ingore it and find the next instance  
                 }
             }
         })
